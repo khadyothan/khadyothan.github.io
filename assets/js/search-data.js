bdadf907490353865c370c168f365ef068e68012
khadyothan <kdasari1@asu.edu>
2025-05-21 02:34:37 +0000

    Deploying to gh-pages from @ khadyothan/khadyothan.github.io@33d7cb702f09703ce93b08b5cab82ddd8dd225e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,44 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of my cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/projects/";
           },
-        },{
+        },{id: "projects-real-time-nyc-cab-trip-processing-pipeline",
+          title: 'Real-time NYC Cab Trip Processing Pipeline',
+          description: "Developed a real-time data ingestion pipeline to feed NYC cab trip data (150+ locations, 5000+ trips) into Kafka and a distributed Neo4j cluster, executing PageRank and BFS for advanced graph insights.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-image-recognition-as-a-service",
+          title: 'Image Recognition as a Service',
+          description: "Architected an on-demand, auto scaling application on AWS to handle 1000+ concurrent image-classification requests in under 60 seconds, comparing a custom EC2-based scaling solution against a serverless Lambda.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-rag-application-for-arxiv-papers",
+          title: 'RAG Application for Arxiv Papers',
+          description: "Built a Retrieval-Augmented Generation (RAG) application for academic papers using Faiss, RocksDB, Llama2, incorporating advanced chunking and re-ranking strategies to enhance retrieval accuracy and response quality.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
