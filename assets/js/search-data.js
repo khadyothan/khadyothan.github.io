5b9ad0689033f0c5159c2af9e13466120a3d0d33
khadyothan <kdasari1@asu.edu>
2025-05-23 00:23:51 +0000

    Deploying to gh-pages from @ khadyothan/khadyothan.github.io@a7a2ccdcc40a58d002182f014265f6cfd2a18453 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,44 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of my cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "projects-real-time-nyc-cab-trip-processing-pipeline",
-          title: 'Real-time NYC Cab Trip Processing Pipeline',
-          description: "Developed a real-time data ingestion pipeline to feed NYC cab trip data (150+ locations, 5000+ trips) into Kafka and a distributed Neo4j cluster, executing PageRank and BFS for advanced graph insights.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-serverless-face-recognition",
-          title: 'Serverless Face Recognition',
-          description: "A serverless face recognition pipeline using AWS Lambda, S3, and PyTorch with Docker-based deployment.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-rag-application-for-arxiv-papers",
-          title: 'RAG Application for Arxiv Papers',
-          description: "Built a Retrieval-Augmented Generation (RAG) application for academic papers using Faiss, RocksDB, Llama2, incorporating advanced chunking and re-ranking strategies to enhance retrieval accuracy and response quality.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{
+  },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -75,8 +38,8 @@ ninja.data = [{
           window.open("https://leetcode.com/u/khadyothan/", "_blank");
         },
       },{
-        id: 'social-letterboxd_url',
-        title: 'Letterboxd_url',
+        id: 'social-letterboxd',
+        title: 'Letterboxd',
         section: 'Socials',
         handler: () => {
           window.open("", "_blank");
